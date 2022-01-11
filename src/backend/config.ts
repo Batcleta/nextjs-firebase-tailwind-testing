92dd1af2b2185e10b854faf456e915730d36619b
Batcleta <thewazig.wf@gmail.com>
2022-01-11 17:22:44 -0300

    finalizado

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -3,9 +3,12 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_APP_ID
     })
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,77 +1,52 @@
 import Head from "next/head";
-import { useState } from "react";
+
 import Botão from "../components/Botão";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
-import Cliente from "../core/Client";
+import useClientes from "../hooks/UseClientes";
 
 export default function Home() {
-  
-  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
-  
 
-  const clientes =[
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 25, '2'),
-    new Cliente('Pedro', 74, '3'),
-    new Cliente('Maria ', 26, '4'),
-    new Cliente('Samira', 45, '5'),
-    new Cliente('Paulo', 54, '6'),
-    new Cliente('Henrique', 34, '7'),
-    new Cliente('Elias', 24, '8'),
-    new Cliente('Arthur', 24, '9'),
-    new Cliente('Larissa', 22, '10'),
-    new Cliente('Carlos', 29, '11'),
-  ]
-
-  function clienteSelecionado(cliente: Cliente){
-    setCliente(cliente)
-    setVisivel('form')
-  }
-
-  function clienteExcluido(cliente: Cliente){
-    console.log(cliente.nome)
-  }
-
-  function novoCliente (){
-    setCliente(Cliente.vazio())
-    setVisivel('form')
-  }
-
-  function salvarCliente(cliente: Cliente){
-    console.log(cliente)
-    setVisivel('tabela')
-  }
+  const {
+    cliente,
+    clientes,
+    novoCliente,
+    salvarCliente,
+    selecionarCliente,
+    excluirCliente,
+    tabelaVisivel,
+    exibirTabela
+  } = useClientes()
 
   return (
-    <div className={`flex justify-center items-center h-screen
-    bg-gradient-to-r from-blue-500 to-blue-900 text-white
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
     `}>
-      <Layout title="Cadastro simples">
-
-        {visivel === 'tabela'? (
+      <Layout title="Cadastro Simples">
+        {tabelaVisivel ? (
           <>
-            <div className="flex justify-end">  
-              <Botão onClick={novoCliente} cor="green" className="mb-4">Novo cliente</Botão>
+            <div className="flex justify-end">
+              <Botão cor="green" className="mb-4"
+                onClick={novoCliente}>
+                Novo Cliente
+              </Botão>
             </div>
-          
-             <Tabela 
-             clientes={clientes} 
-             clienteSelecionado={clienteSelecionado}
-             clienteExcluido={clienteExcluido}
-             />
+            <Tabela clientes={clientes}
+              clienteSelecionado={selecionarCliente}
+              clienteExcluido={excluirCliente}
+            />
           </>
-        ): (
-          <Formulario 
-          cliente={cliente} 
-          clienteMudou={salvarCliente}
-          cancelado={() => setVisivel('tabela')}
+        ) : (
+          <Formulario
+            cliente={cliente}
+            clienteMudou={salvarCliente}
+            cancelado={exibirTabela}
           />
         )}
-     
       </Layout>
     </div>
-  );
+  )
 }
